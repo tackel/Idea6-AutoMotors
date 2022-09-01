325c5c434e29479d229c22e339087da73520b8d8
Fernando <fernandoverri80@gmail.com>
2022-08-31 22:01:14 -0300

    cambios

--- extraccion_datos.py
+++ extraccion_datos.py
@@ -11,13 +11,14 @@ def read_ventas():
 
 def normalizacion_empleados():
     df_empleados = pd.read_csv(f'{path}/Empleados.csv', sep=';')
-    print(df_empleados.info())
-    print(df_empleados.describe())
+    print(df_empleados.head())
+    #print(df_empleados.info())
+    #print(df_empleados.describe())
     print(df_empleados.dtypes)
-    print(df_empleados.isna())
-    print(df_empleados.drop_duplicates())
-    print(df_empleados.iloc[:,1])
-    print(df_empleados.value_counts())
+    #print(df_empleados.isna())
+    #print(df_empleados.drop_duplicates())
+    #print(df_empleados.iloc[:,1])
+    #print(df_empleados.value_counts())
     print('Data set Empleados no requiere cambios')
 
 def normalizacion_ventas():
@@ -28,12 +29,21 @@ def normalizacion_ventas():
     #print(df_ventas2)
     #print(df_ventas2.isna())
     df_ventas2 = df_ventas2.drop(index=[127265, 127255])
+    #print(df_ventas2.dtypes)
+    #print(df_ventas2.Cantidad == '1,006')
+    print('------')
+    df_ventas2['Empleado'] = pd.to_numeric(df_ventas2.Empleado, errors= 'raise')
+    df_ventas2.Fecha = pd.to_datetime(df_ventas2['Fecha'], format='%d/%m/%Y')
+    
+    df_ventas2['Cantidad'] = pd.to_numeric(df_ventas2['Cantidad'], errors='coerce')
+    print(df_ventas2.iloc[107700:107705,8].isna())
+
+    #df_ventas2.Ventas = df_ventas2['Ventas'].astype(float, errors= 'raise')
     print(df_ventas2.head())
+    #print(df_ventas2[['Fecha','CodigoFamilia','Familia','Cantidad','Ventas']])
     print(df_ventas2.dtypes)
   
   
-
-
 
 
 #normalizacion_empleados()
