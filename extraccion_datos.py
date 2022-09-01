8cc672d84a496f20b83b4021ab07196b9af71f94
Fernando Verri <74513344+tackel@users.noreply.github.com>
2022-09-01 15:18:01 -0300

    guardado de archivo limpio en csv

--- extraccion_datos.py
+++ extraccion_datos.py
@@ -22,26 +22,49 @@ def normalizacion_empleados():
     print('Data set Empleados no requiere cambios')
 
 def normalizacion_ventas():
-    
+    # Obtener loa datos del archivo csv
     df_ventas = pd.read_csv(f'{path}/Ventas.csv', sep=';', low_memory=False)
     #print(df_ventas.info())
-    df_ventas2 = df_ventas.drop_duplicates()
+    # Eliminar registros duplicados
+    df_ventas = df_ventas.drop_duplicates()
+    # Eliminar registros nan
+    df_ventas = df_ventas.dropna()
     #print(df_ventas2)
-    #print(df_ventas2.isna())
-    df_ventas2 = df_ventas2.drop(index=[127265, 127255])
-    #print(df_ventas2.dtypes)
+
+    # pasar columna empleados a numerico
+    df_ventas['Empleado'] = df_ventas['Empleado'].astype('int', errors= 'raise')
+    # pasar columna fecha a formato fecha
+    df_ventas.Fecha = pd.to_datetime(df_ventas['Fecha'], format='%d/%m/%Y')
+
+    #pasar columna candad a numerico
+    df_ventas['Cantidad'] = pd.to_numeric(df_ventas['Cantidad'], errors='coerce')
+    # obtengo la media para pasar los cambiar los nan
+    mean_df = df_ventas['Cantidad'].mean()
+    # Cambio nan por el valor medio
+    df_ventas.fillna(mean_df, inplace=True)
+    # Cambio columna canidad a entero
+    df_ventas['Cantidad'] = df_ventas['Cantidad'].astype('int', errors= 'raise')
+
+    # Pasar columna Ventas a float
+    # filtrar comillas y comas de la columna ventas.
+    df_ventas['Ventas'] = df_ventas['Ventas'].apply(lambda x : x[:x.find(',')].replace(".",""))
+    # Lugo  pasar columna ventas a entero
+    df_ventas['Ventas'] = pd.to_numeric(df_ventas['Ventas'], errors='raise')
+
+    #df_ventas2 = df_ventas2.drop(index=[127265, 127255])
     #print(df_ventas2.Cantidad == '1,006')
-    print('------')
-    df_ventas2['Empleado'] = pd.to_numeric(df_ventas2.Empleado, errors= 'raise')
-    df_ventas2.Fecha = pd.to_datetime(df_ventas2['Fecha'], format='%d/%m/%Y')
     
-    df_ventas2['Cantidad'] = pd.to_numeric(df_ventas2['Cantidad'], errors='coerce')
-    print(df_ventas2.iloc[107700:107705,8].isna())
+    #df_ventas2['Empleado'] = pd.to_numeric(df_ventas2.Empleado, errors= 'raise')
+    #df_ventas2.Fecha = pd.to_datetime(df_ventas2['Fecha'], format='%d/%m/%Y')
+    
+    #df_ventas2['Cantidad'] = pd.to_numeric(df_ventas2['Cantidad'], errors='coerce')
+    #print(df_ventas2.iloc[107700:107705,8].isna())
 
     #df_ventas2.Ventas = df_ventas2['Ventas'].astype(float, errors= 'raise')
-    print(df_ventas2.head())
+
     #print(df_ventas2[['Fecha','CodigoFamilia','Familia','Cantidad','Ventas']])
-    print(df_ventas2.dtypes)
+    #print(df_ventas2.dtypes)
+    df_ventas.to_csv(f'{path}/empleados_limpio.csv')
   
   
 
